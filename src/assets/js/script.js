3020ec4a608e10b4c2f273daa732a8a26af9b95c
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-08-12 18:21:07 +0200

    update

---
+++ src/assets/js/script.js
@@ -0,0 +1,7 @@
+let btnShare = document.getElementById("js-btn");
+let btnPopup = document.getElementById("js-btn-popup");
+
+function togglePopup() {
+	btnPopup.classList.toggle("active");
+	btnShare.classList.toggle("active");
+}
