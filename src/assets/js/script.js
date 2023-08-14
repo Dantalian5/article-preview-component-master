389967794f92720c92755b56202c47155c30b25e
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2023-08-14 11:01:57 +0200

    fix state

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,9 @@
 let btnShare = document.getElementById("js-btn");
+//let ctaShare = document.getElementById("js-share-cta");
 let btnPopup = document.getElementById("js-btn-popup");
 
 function togglePopup() {
 	btnPopup.classList.toggle("active");
 	btnShare.classList.toggle("active");
+	//ctaShare.classList.toggle("active");
 }
